ee75f91a408eeb8c233376fd4ae96f794ce53785
Derik Strattan <derik.strattan@gmail.com>
2013-11-17 20:17:10 -0800

    updated decoder to use protect

--- spec/decoderSpec.js
+++ spec/decoderSpec.js
@@ -2,6 +2,6 @@ describe("Decoder", function() {
   it("decodes an encrypted string from the URL", function()) {
     decoder.getCypher = function() { decoder.cypher = CryptoJS.AES.encrypt("a test string", "password"); };
     decoder.getPassword    = function() { decoder.password = "password"; };
-    expect(decoder.decrypt()).toEqual("a test string");
+    expect(decoder.protect()).toEqual("a test string");
   });
 });
