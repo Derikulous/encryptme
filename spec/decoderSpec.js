3dccaf9ea7c6cb1c751038984bb15c29852befbe
Derik Strattan <derik.strattan@gmail.com>
2013-11-17 20:06:27 -0800

    Decoder now providing url. Changed spec to call cypher.

--- spec/decoderSpec.js
+++ spec/decoderSpec.js
@@ -1,6 +1,6 @@
 describe("Decoder", function() {
   it("decodes an encrypted string from the URL", function()) {
-    descoder.getCypherText = function() { decoder.cypherText = CryptoJS.AES.encrypt("a test string", "password"); };
+    decoder.getCypher = function() { decoder.cypher = CryptoJS.AES.encrypt("a test string", "password"); };
     decoder.getPassword    = function() { decoder.password = "password"; };
     expect(decoder.decrypt()).toEqual("a test string");
   });
--- src/decoder.js
+++ src/decoder.js
@@ -1,30 +1,29 @@
-<script>
 var decoder = {
   getCypher: function() {
-    this.cypher = document.URL.slice(window.location.toString().indexOf("?")+1)
+    this.cypher = document.URL.split('?')[1]
   },
   getPassword: function() {
     this.password = prompt("Enter a password.")
     console.log(this.password)
           // A user types in a password
           // Then clicks lock down.
-        },
-        setUrl: function() {
-          this.url = decoder.decrypt()
-          alert(this.url)
-          // A URL is given in the dialog box
-        },
-        decrypt: function() {
-          cypher = CryptoJS.AES.decrypt(this.cypher, this.password).toString(CryptoJS.enc.Utf8)
-          return cypher
-        },
-        protect: function() {
-          this.getCypher()
-          this.getPassword()
-          this.setUrl()
-        }
-      }
-      document.getElementById("decrypt").onclick = function(){
-        decoder.protect()
-      }
-</script>
+  },
+  setUrl: function() {
+    this.url = decoder.decrypt()
+    alert(this.url)
+    // A URL is given in the dialog box
+  },
+  decrypt: function() {
+    cypher = CryptoJS.AES.decrypt(this.cypher, this.password).toString(CryptoJS.enc.Utf8)
+    return cypher
+  },
+  protect: function() {
+    this.getCypher()
+    this.getPassword()
+    this.setUrl()
+  }
+}
+document.getElementById("decrypt").onclick = function(){
+  decoder.protect()
+}
+
